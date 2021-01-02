ada339ef2ff23375fc6b6d22d1d1260040db8548
yangfan19 <yangfan19@100tal.com>
2021-01-02 21:28:18 +0800

    配置postcss.config文件

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -26,12 +26,6 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const postcssAspectRatioMini = require('postcss-aspect-ratio-mini');
-const postcssPxToViewport = require('postcss-px-to-viewport');
-const postcssWriteSvg = require('postcss-write-svg');
-const postcssCssnext = require('postcss-cssnext');
-const postcssViewportUnits = require('postcss-viewport-units');
-const cssnano = require('cssnano');
 const postcssNormalize = require('postcss-normalize');
 
 const appPackageJson = require(paths.appPackageJson);
@@ -114,39 +108,6 @@ module.exports = function (webpackEnv) {
           // Necessary for external CSS imports to work
           // https://github.com/facebook/create-react-app/issues/2677
           ident: 'postcss',
-          plugins: () => [
-            require('postcss-flexbugs-fixes'),
-            require('postcss-preset-env')({
-              autoprefixer: {
-                flexbox: 'no-2009',
-              },
-              stage: 3,
-            }),
-            postcssAspectRatioMini({}),
-            postcssPxToViewport({
-              viewportWidth: 750, // (Number) The width of the viewport.
-              viewportHeight: 1334, // (Number) The height of the viewport.
-              unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to.
-              viewportUnit: 'vw', // (String) Expected units.
-              selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
-              minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
-              mediaQuery: false, // (Boolean) Allow px to be converted in media queries.
-            }),
-            postcssWriteSvg({
-              utf8: false,
-            }),
-            postcssCssnext({}),
-            postcssViewportUnits({}),
-            cssnano({
-              preset: 'advanced',
-              autoprefixer: false,
-              'postcss-zindex': false,
-            }),
-            // Adds PostCSS Normalize as the reset css with default options,
-            // so that it honors browserslist config in package.json
-            // which in turn let's users customize the target behavior as per their needs.
-            postcssNormalize(),
-          ],
           sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment,
         },
       },
--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from 'react';
+// import { Checkbox, Modal, List } from 'antd-mobile';
+import { Checkbox, List, Modal } from 'antd-mobile';
+const { CheckboxItem } = Checkbox;
 function App() {
+  const [checkboxValue, setCheckboxValue] = useState(false); // 优秀作业的选中状态
+  const [visible, setVisible] = useState(false); // 遮罩层
+
+  useEffect(() => {
+    if (checkboxValue) {
+      setVisible(true);
+    }
+  }, [checkboxValue]);
+
+  // 关闭遮罩层
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
     <div className='App'>
-      <h2>这是HTML5</h2>
+      <List>
+        <CheckboxItem checked={checkboxValue} onClick={() => setCheckboxValue(!checkboxValue)}>
+          已选为优秀作业
+        </CheckboxItem>
+      </List>
+      <Modal
+        popup
+        afterClose={() => {
+          alert('afterClose');
+        }}
+        visible={visible}
+        onClose={onClose}
+        animationType='slide-up'
+      >
+        <p>1111111111</p>
+      </Modal>
     </div>
   );
 }
